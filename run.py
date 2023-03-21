ea477391413a58964a49d7bd94de6bb5d046ce4a
John McGrath <johnpmcgrath1996@gmail.com>
2023-03-21 00:41:33 +0000

    Add time import and introduction

--- run.py
+++ run.py
@@ -1,1 +1,15 @@
-print("Hello World!")
+import time
+
+
+def start():
+    """
+    Method to start the game
+    """
+    name = input("What is your name? ")
+    print(f'Hello, {name}, lets play hangman!')
+    time.sleep(2)
+    print("The game is starting!\nTime to  play Hangman!")
+    time.sleep(3)
+
+
+start()
