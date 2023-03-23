283d1f7026cc87d75f002a4db465741306b79f98
John McGrath <johnpmcgrath1996@gmail.com>
2023-03-23 17:35:53 +0000

    Add function to take player input to guess a letter

--- run.py
+++ run.py
@@ -1,5 +1,26 @@
 import time
 
+secret_word = ("secret")
+turns = 10
+
+
+def get_guess():
+    """
+    This function takes a guess from the player
+    """
+    while True:
+        guess = input('Guess a letter: ')
+        guess = guess.lower()
+
+        if len(guess) != 1:
+            print('Please enter a single letter.')
+        # elif guess in alreadyGuessed:
+        #    print('You have already guessed that letter. Choose again.')
+        elif guess not in 'abcdefghijklmnopqrstuvwxyz':
+            print('Please enter a LETTER.')
+        else:
+            return guess
+
 
 def start():
     """
@@ -10,6 +31,7 @@ def start():
     time.sleep(2)
     print("The game is starting!\nTime to  play Hangman!")
     time.sleep(3)
+    get_guess()
 
 
 start()
