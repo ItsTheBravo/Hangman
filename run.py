b020e6391be5aa619ad912d40e839196694d737f
John McGrath <johnpmcgrath1996@gmail.com>
2023-03-25 19:53:09 +0000

    Add check win and end game functions

--- run.py
+++ run.py
@@ -33,7 +33,48 @@ def display_word():
             word += letter
         else:
             word += "_"
-        print(word)
+    print(word)
+
+
+def check_win():
+    """
+    This function checks if the player has won the game
+    """
+    for letter in secret_word:
+        if letter not in correct_guesses:
+            return False
+    return True
+
+
+def play_again():
+    """
+    This function asks the player if they want to play again
+    """
+    while True:
+        answer = input("Do you want to play again? (y/n) ")
+        answer = answer.lower()
+
+        if answer == "y":
+            return True
+        elif answer == "n":
+            return False
+        else:
+            print("Please enter y or n.")
+
+
+def end_game(win):
+    """
+    This function ends the game and displays the result
+    """
+    if win:
+        print("Congratulations, you won!")
+    else:
+        print(f"Sorry, you lost. The word was {secret_word}.")
+        time.sleep(2)
+    if play_again():
+        start()
+    else:
+        print("Thanks for playing!")
 
 
 def start():
@@ -53,6 +94,9 @@ def start():
         if guess in secret_word:
             correct_guesses.append(guess)
             print("Correct!")
+            if check_win():
+                end_game(True)
+                return
         else:
             print("Incorrect!")
             turns -= 1
